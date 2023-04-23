cdd7411caccdf7f84541d5ac0388ac6de2bcb8dd
Phumzy123 <funmilayoawe100@gmail.com>
2023-04-23 04:11:19 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int lowercase = 'b';
+int uppercase = 'B';
+while (lowercase = 'b')
+{
+putchar(lowercase);
+(lowercase) = 'b';
+}
+while (uppercase = 'B')
+{
+putchar(uppercase);
+(uppercase) = 'B';
+}
+putchar["\n"]
+return (0);
+}
