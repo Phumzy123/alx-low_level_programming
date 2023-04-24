ae403b72d450b67294766f407f9800f14206c332
Phumzy123 <funmilayoawe100@gmail.com>
2023-04-24 05:56:00 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@
 
 int main(void)
 {
-	char lowercase, e, q;
+	char lowercase;
 
 	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
-		if (lowercase != e && lowercase != q)
+		if (lowercase != 'e' && lowercase != 'q')
 			putchar(lowercase);
 	}
 	putchar('\n');
