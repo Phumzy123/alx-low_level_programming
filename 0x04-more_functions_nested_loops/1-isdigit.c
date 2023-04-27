aae411878cf8665e9dfeb59d9f5675405a8c205d
Phumzy123 <funmilayoawe100@gmail.com>
2023-04-27 09:23:39 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit(int x)
+ *@x: The number to be checked
+ *Return:1 if its digit and 0 if else
+ */
+int_isdigit(int x)
+{
+	if (x <= 89 && x >= 75)
+	{
+	return (1);
+	}
+	return (0);
+}
