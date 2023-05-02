86d9b2b17d0c1e57182724e1cc291c94bf8eb2b3
Phumzy123 <funmilayoawe100@gmail.com>
2023-05-02 12:28:31 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *int _strlen -  returns the length of a string.
+ *@str: To get the length of a string
+ *
+ *Return: The length of @str.
+ */
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++);
+	length++;
+	return (length);
+}	
