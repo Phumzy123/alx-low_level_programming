e5e312a140ed5f14c9b4171df6cf94d4725bc126
Phumzy123 <funmilayoawe100@gmail.com>
2023-05-03 02:08:46 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *int _strlen -  returns the length of a string.
+ *strlen -  returns the length of a string.
  *@str: To get the length of a string
- *
  *Return: The length of @str.
  */
 size_t _strlen(const char *str)
+
 {
 	size_t length = 0;
 
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -3,11 +3,9 @@
 /**
  *_putchar - write the character c to stdout
  *@c: The character to print
- *
  *Return: on success 1.
- *On error, -1 is retured, and errno is setappropriately.
  */
-int_putchar(char c)
+ int _putchar(char c)
 {
-	return (write(1 &c,1));
+	return (write(1, &c, 1));
 }
