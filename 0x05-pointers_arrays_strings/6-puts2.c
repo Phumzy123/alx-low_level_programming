f8ac6cbbcca7f76fe57d2209f54992e4e2ba0a21
Phumzy123 <funmilayoawe100@gmail.com>
2023-05-03 09:56:32 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,12 +12,14 @@ void puts2(char *str)
 	int i = 0;
 	int j = 0;
 
-	while (str[j] != '\0') j++;
-	
+	while (str[j] != '\0')
+
+	j++;
+
 	for (i = 0; i < j; i += 2)
 			{
 		_putchar(str[i]);
 		}
-		
+
 		_putchar('\n');
 }
