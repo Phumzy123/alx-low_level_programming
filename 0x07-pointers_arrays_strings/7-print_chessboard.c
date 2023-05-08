1265e07cf26e0c8e951ef36b967c295eda058187
Phumzy123 <funmilayoawe100@gmail.com>
2023-05-08 06:39:09 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *print_chessboard -  function that prints the chessboard
+ *@c: Input
+ *Return: Always 0 (success)
+ */
+void print_chessboard(char)
+{
+	char string1[] = "rkbqkbkr";
+	char string2[] = "pppppppp";
+	{
+		printf("%s\n", string1);
+		printf("%s\n", string2);
+	}
+}
+
