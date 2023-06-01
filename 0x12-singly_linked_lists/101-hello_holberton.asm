6a97b7d433ac714e8ecf74718394d462c189aada
Phumzy123 <funmilayoawe100@gmail.com>
2023-06-01 05:42:12 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,15 @@
+section .data
+    hello db 'hello,holberton',0Ah   ; message to be displayed
 
+_start:
+    ; write hello to stdout
+    mov eax, 4       ; system call for write
+    mov ebx, 1       ; file descriptor for stdout
+    mov ecx, hello   ; message to be printed
+    mov edx, 14      ; message length
+    int 0x80         ; invoke kernel
+
+    ; exit program
+    mov eax, 1       ; system call for exit
+    xor ebx, ebx     ; return 0 status
+    int 0x80         ; invoke kernel
